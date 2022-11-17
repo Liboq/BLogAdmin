dbf0233b32c01b53581eda3aa4d295902f632c97
Liboq <99778162+Liboq@users.noreply.github.com>
2022-11-17 11:03:08 +0800

    feat：导出md按钮

--- src/components/markdown.jsx
+++ src/components/markdown.jsx
@@ -1,24 +1,42 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import Posts from "./post";
 import ViewMd from "./view";
 
-import './style/github-dark.css';
-import './style/index.css';
+import "./style/github-dark.css";
+import "./style/index.css";
 
 function Markdown() {
   const [text, setText] = useState("");
- 
+  const funDownload = function (content) {
+    // 创建隐藏的可下载链接
+    var eleLink = document.createElement("a");
+    const filename = new Date().getTime();
+    eleLink.download = filename + ".md";
+    eleLink.style.display = "none";
+    // 字符内容转变成blob地址
+    var blob = new Blob([content]);
+    eleLink.href = URL.createObjectURL(blob);
+    // 触发点击
+    document.body.appendChild(eleLink);
+    eleLink.click();
+    // 然后移除
+    document.body.removeChild(eleLink);
+  };
+
   return (
     <>
-    <div className="marked-head">welcome to markdown edit</div>
-    <div className="marked">
-
-      <Posts
-      setText={setText}
-       
-      ></Posts>
-      <ViewMd   text ={text} ></ViewMd>
-    </div>
+      <div className="marked-head">
+        <span>welcome to markdown edit</span>{" "}
+        <span className="abs-btn">
+          <button onClick={() => funDownload(text)} className="load-btn">
+            导出为md文件
+          </button>{" "}
+        </span>{" "}
+      </div>
+      <div className="marked">
+        <Posts setText={setText}></Posts>
+        <ViewMd text={text}></ViewMd>
+      </div>
     </>
   );
 }
