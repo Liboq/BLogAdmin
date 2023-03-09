3176993a3ea55754e3accf499a6fde26890e11df
Liboq <99778162+Liboq@users.noreply.github.com>
2023-03-09 13:20:58 +0800

    finish:用户管理

--- src/pages/layout/resource/user/index.js
+++ src/pages/layout/resource/user/index.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-import { getAllUser } from "../../../../request/user";
-import { Button, Select, Space, Table, Tag, Form, Input } from "antd";
-import { transformTree } from "../../../../utils/local";
-import { permmsionList } from "../../../../utils/permission";
+import { getAllUser, updateUserRole } from "../../../../request/user";
+import { Button, Select, Space, Table, Tag, Form, Input, Modal, message } from "antd";
+import { getRoles } from "../../../../request/role";
 
 const UserManage = () => {
   const [data, setData] = useState([]);
@@ -10,7 +9,6 @@ const UserManage = () => {
   const getAllUsers = async () => {
     const res = await getAllUser();
     if (res.status === 200) {
-      console.log(res);
       setData(res.data.users);
     }
   };
@@ -19,22 +17,57 @@ const UserManage = () => {
   }, []);
   return (
     <>
-      <DataTable data={data}></DataTable>
+      <DataTable getAllUsers={getAllUsers}  data={data}></DataTable>
     </>
   );
 };
-const Modal = () => {
-  const [modal2Open, setModal2Open] = useState(false);
-  const selctData = transformTree(permmsionList);
-  const updateUserRole = () => {};
+const Dialog = (props) => {
+  const [selectedPermission,setSelectedPermission] = useState([])
+  const [roleList,setRoleList] =useState([])
+  const getRoleList = async ()=>{
+    const res = await getRoles()
+    const initRoleList = res.data.map(item=>({
+      value:item.name,
+      label:item.name
+    }))
+    setRoleList(initRoleList)
+  }
+  const initPermission = props.selectRow.role.map(item=>{
+    return {
+      value:item,
+      label:item
+    }
+  })
+  const handleChange = (value)=>{
+    setSelectedPermission(value)
+  }
+  const updateUserRoles = async() => {
+    
+    const params = {
+      _id:props.selectRow._id,
+      role:selectedPermission
+    }
+    const res = await updateUserRole(params)
+    if(res.status === 200){
+      message.success(res.message)
+      props.setModal2Open(false)
+      props.getAllUsers()
+    }else{
+      message.error(res.message)
+    }
+
+  };
+  useEffect(()=>{
+    getRoleList()
+  },[])
   return (
     <>
       <Modal
         title="新增角色"
         centered
-        open={modal2Open}
+        open={props.modal2Open}
         footer={null}
-        onCancel={() => setModal2Open(false)}
+        onCancel={() => props.setModal2Open(false)}
         destroyOnClose
       >
         <Form
@@ -44,14 +77,13 @@ const Modal = () => {
           style={{ maxWidth: 600 }}
           initialValues={{ remember: true }}
           autoComplete="off"
-          onFinish={updateUserRole}
+          onFinish={updateUserRoles}
         >
           <Form.Item
-            label="name"
-            name="name"
-            rules={[{ required: true, message: "Please input your username!" }]}
+            label="昵称"
+            name="昵称"
           >
-            <Input />
+            <Input disabled defaultValue={props.selectRow.userName} />
           </Form.Item>
 
           <Form.Item
@@ -61,12 +93,22 @@ const Modal = () => {
               { required: true, message: "Please input your description!" },
             ]}
           >
-            <Select></Select>
+            <Select
+              mode="tags"
+              style={{
+                width: "100%",
+              }}
+              showArrow
+              placeholder="角色"
+              options={roleList}
+              defaultValue={initPermission}
+              onChange={handleChange}
+            />
           </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Button type="primary" htmlType="submit">
-              新增角色
+              确认编辑
             </Button>
           </Form.Item>
         </Form>
@@ -75,50 +117,60 @@ const Modal = () => {
   );
 };
 
-const columns = [
-  {
-    title: "昵称",
-    dataIndex: "userName",
-    key: "userName",
-  },
-  {
-    title: "手机号码",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "角色",
-    dataIndex: "role",
-    key: "role",
-    render: (_, { role }) => (
-      <>
-        {role.map((tag) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          return (
-            <Tag color={color} key={tag}>
-              {tag}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: "动作",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <Button>编辑</Button>
-      </Space>
-    ),
-  },
-];
-
-const DataTable = (props) => (
-  <Table
-    rowKey={(record) => record.userName}
-    columns={columns}
-    dataSource={props.data}
-  />
-);
+const DataTable = (props) => {
+  const handleEdit = (row) => {
+    setSelectRow(row)
+    setModal2Open(true);
+  };
+  const [modal2Open, setModal2Open] = useState(false);
+  const [selectRow,setSelectRow] = useState({})
+  const columns = [
+    {
+      title: "昵称",
+      dataIndex: "userName",
+      key: "userName",
+    },
+    {
+      title: "手机号码",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "角色",
+      dataIndex: "role",
+      key: "role",
+      render: (_, { role }) => (
+        <>
+          {role.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            return (
+              <Tag color={color} key={tag}>
+                {tag}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "动作",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button onClick={() => handleEdit(record)}>编辑</Button>
+        </Space>
+      ),
+    },
+  ];
+  return (
+    <>
+      <Table
+        rowKey={(record) => record.userName}
+        columns={columns}
+        dataSource={props.data}
+      />
+      {modal2Open&&<Dialog getAllUsers={props.getAllUsers} selectRow={selectRow} modal2Open={modal2Open} setModal2Open={setModal2Open}></Dialog>}
+    </>
+  );
+};
 export default UserManage;
--- src/request/user.js
+++ src/request/user.js
@@ -7,3 +7,6 @@ export const getUserIp = ()=>{
 export const getAllUser = ()=>{
     return request('get','user/getAllUser',{})
 }
+export const updateUserRole =(params)=>{
+    return  request('post','user/updateUserRole',params)
+}
